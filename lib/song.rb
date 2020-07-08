9303052540aa22a4350d91eb8805a6d1f47d60b9
Sean Scamehorn <smscamehorn45@gmail.com>
2020-07-08 00:23:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,2 +1,17 @@
 class Song 
+  @@count
   
+def self.count
+@@count
+end
+
+@@genres = []
+
+def self.genres
+  @@genres.uniq 
+end
+
+def self.genre_count
+  genre_count = {}
+  @@genres.each do |genre|
+    if genre_count{
